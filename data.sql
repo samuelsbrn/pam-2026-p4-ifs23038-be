7240ae3cdefdcc3d41cbfad3d7596a8b94fec7a9
samuelsbrn <142556206+samuelsbrn@users.noreply.github.com>
2026-03-03 13:43:00 +0700

    please

--- data.sql
+++ data.sql
@@ -1,10 +1,2 @@
-CREATE TABLE IF NOT EXISTS plants (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nama VARCHAR(100) NOT NULL,
-    path_gambar VARCHAR(255) NOT NULL,
-    deskripsi TEXT NOT NULL,
-    manfaat TEXT NOT NULL,
-    efek_samping TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
+INSERT INTO motors (id, nama, path_gambar, deskripsi, spesifikasi, harga, created_at, updated_at)
+VALUES ('...', '...', '...', '...', 'Spesifikasi mesin...', '15000000', '...', '...');
